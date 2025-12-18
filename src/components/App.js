17e8538d79f0fe2894cfb6680db576b81a1eef17
nithinbairu <nithinbairu08@gmail.com>
2025-12-18 15:43:37 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
-
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
 import './../styles/App.css';
 import { decrement, increment } from "./redux/CounterActions";
+import { store } from "./redux/Store";
 
-const App = () => {
-  const count=useSelector((state)=>state.count);
-  const disptach=useDispatch()
+const Counter = () => {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h1>{count}</h1>
-        <button onClick={()=>disptach(increment())}>Increment</button>
-        <button onClick={()=>disptach(decrement())}>Decrement</button>
+      {/* Do not remove the main div */}
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { store } from "./components/redux/Store";
 
 
 ReactDOM.render(
-    <Provider store={store}>
+    
         <App />
-    </Provider>
+    
     , document.getElementById("root"));
